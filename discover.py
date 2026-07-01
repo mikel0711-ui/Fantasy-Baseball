b296a644e92f6279d7f2ffefabe82f77f784b80a
mikel0711-ui <mikel0711@gmail.com>
2026-07-01 14:18:30 -0500

    Refactor league methods display to use inspect module

--- discover.py
+++ discover.py
@@ -1,8 +1,8 @@
 from modules.league import load_league
+import inspect
 
 league = load_league()
 
-print("League methods:")
-for item in dir(league):
-    if not item.startswith("_"):
-        print(item)
+print(inspect.signature(league.free_agents))
+print()
+print(league.free_agents.__doc__)
