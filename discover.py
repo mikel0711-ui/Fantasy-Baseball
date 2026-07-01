cfddbb3a3292589cd2824f9c5d2bca7b09aed700
mikel0711-ui <mikel0711@gmail.com>
2026-07-01 14:36:50 -0500

    Change player attributes display to dictionary format
    Replaced player attributes printout with player dictionary representation.

--- discover.py
+++ discover.py
@@ -1,18 +1,10 @@
 from modules.league import load_league
+from pprint import pprint
 
 league = load_league()
 
 player = league.free_agents(size=1)[0]
 
-print("PLAYER ATTRIBUTES")
-print("-----------------")
-
-for attr in sorted(dir(player)):
-    if attr.startswith("_"):
-        continue
-
-    try:
-        value = getattr(player, attr)
-        print(f"{attr}: {value}")
-    except Exception:
-        pass
+print("PLAYER DICTIONARY")
+print("=" * 50)
+pprint(player.__dict__)
