9f6d0bf54cfa9bb47c03d699a7788311b52a6fbb
mikel0711-ui <mikel0711@gmail.com>
2026-07-01 17:34:23 -0500

    Implement team lookup and roster display
    Added functionality to find and display team attributes and roster for a specific team.

--- discover.py
+++ discover.py
@@ -3,8 +3,22 @@ from pprint import pprint
 
 league = load_league()
 
-player = league.free_agents(size=1)[0]
+# Find your team
+for team in league.teams:
+    if team.team_name == "Shitty Mike":
 
-print("PLAYER DICTIONARY")
-print("=" * 50)
-pprint(player.__dict__)
+        print("=" * 80)
+        print("TEAM ATTRIBUTES")
+        print("=" * 80)
+
+        pprint(team.__dict__)
+
+        print("\n")
+        print("=" * 80)
+        print("ROSTER")
+        print("=" * 80)
+
+        for player in team.roster:
+            print(player.name)
+
+        break
