750a0b4f1663c7526b72d380037a1b0dc8665442
mikel0711-ui <mikel0711@gmail.com>
2026-07-02 13:23:00 -0500

    Load league data and print team attributes

--- discover.py
+++ discover.py
@@ -1,5 +1,9 @@
 from config import MY_TEAM
+from modules.league import load_league
+
+league = load_league()
 
 my_team = next(t for t in league.teams if t.team_name == MY_TEAM)
+
 print("Team-level attributes:")
 print([attr for attr in dir(my_team) if not attr.startswith("_")])
